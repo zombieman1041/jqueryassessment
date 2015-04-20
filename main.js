d50ae179071cd535da39644888475984d7f1bef4
Blake Dayman <daymanblake@gmail.com>
2015-04-20 13:33:30 -0700

    Done!

---
+++ main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+	$('.dan').click(function(){
+	alert("hey you clicked on me!");
+});
+});	
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	alert("hey you clicked on me again!");
+});
+});
